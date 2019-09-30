47458130f333d014785664f67b30a4effc085045
John Williams <darumswizel@gmail.com>
2019-09-29 21:42:27 -0500

    CSV looking good. Onto downloading the films.

--- flickscrape.py
+++ flickscrape.py
@@ -1,16 +1,19 @@
-import os, sys, re, csv, requests, lxml, urllib3
+import os, sys, re, math, csv, requests 
+import lxml
 from bs4 import BeautifulSoup
+from hurry.filesize import size
 
 sovietMovies = []
 skippedFlicks = []
 base = "https://sovietmoviesonline.com/"
 link = base + "all_movies.html"
 
-class Flick():
-    badFile = 1
-    badSrt = 1
-    fileSize = 0.01
-    srtSize = 0.01
+class Flick:
+    badFile = True
+    badSrt = True
+    fileSize = 0.0
+    srtSize = 0.0
+    title = ""
     downloadUrl = ""
     srtUrl = ""
     name = ""
@@ -18,7 +21,7 @@ class Flick():
     imdb = ""
     og = ""
     director = ""
-
+    
     def __init__(self, url, num):
         self.url = url
         self.num = num
@@ -26,10 +29,13 @@ class Flick():
     def __str__(self):
         string = (
             "URL: " + self.url + "\n" + 
+            "Title: " + self.title + "\n" +
             "Download Url: " + self.downloadUrl + "\n" +
             "Sub Url: " + self.srtUrl + "\n" +
             "File Size: " + str(self.fileSize / 1000000000) + " gb\n" +
-            "SRT Size: " + str(self.fileSize) + "\n" +
+            "SRT Size: " + str(self.fileSize / 1000) + " mb\n" +
+            "Bad File: " + str(self.badFile) + "\n" +
+            "Bad SRT: " + str(self.badSrt) + "\n" +
             "Original Name: " + self.og + "\n" +
             "Year: " + self.year + "\n" +
             "IMDB: " + self.imdb + "\n" +
@@ -37,32 +43,71 @@ class Flick():
             )
         return string
 
+def csvOut(flick):
+    csvString = ""
+    csvString += flick.title + "|"
+    csvString += flick.year + "|"
+    csvString += flick.director + "|"
+    csvString += flick.imdb + "|"
+    csvString += flick.og + "|"
+    csvString += str(flick.fileSize / 1000000000) + "|"
+    csvString += str(flick.srtSize / 1000) + "|"
+    csvString += flick.url + "|"
+    csvString += flick.downloadUrl + "|"
+    csvString += flick.srtUrl + "|"
+    csvString += str(flick.badFile) + "|"
+    csvString += str(flick.badSrt) + "\n"
+    return csvString
+
 def errMsg(msg):
     sys.stderr.write(msg + "\n")
     sys.stderr.flush()
 
+def downloadFlicks():
+    print("woah there buster")
+
+#Creates CSV file containg film information
+#Defaults to this script's directory
 def writeCsv():
-    print("not yet")
+    d = os.path.realpath(__file__)
+    scriptDir = d.rsplit('/', 1)
+    scriptDir = scriptDir[0]
+    csvName = scriptDir + "/flickscrape-output"
+    x = 1
+    csvPath = csvName + ".csv"
+    while True:
+        if os.path.exists(csvPath):
+            csvPath = csvName + "_" + str(x) + ".csv"
+            x += 1
+        else:
+            break
+    f = open(csvPath, "w")
+    startLine = "Title|Year|Director|IMDb Score|Russian Title|Flick Size (in GB)|SRT Size (KB)|Flick URL|Download url|SRT url|Bad File|Bad Subtitles\n"
+    f.write(startLine)
+    for flick in sovietMovies:
+        print(csvOut(flick))
+        f.write(csvOut(flick))
+    for flick in skippedFlicks:
+        flick.fileSize = 0
+        flick.srtSize = 0
+        print(csvOut(flick))
+        f.write(csvOut(flick))
 
 #Flicks are initialized to not download.
 #Cuts redundancy setting Flick
 def downloadInit(flick):
-    r = requests.get(flick.downloadUrl)
-    if r.status_code == requests.codes.ok:
-        flickHead = requests.head(flick.downloadUrl)
-        srtHead = requests.head(flick.srtUrl)
-        try:
-            flick.srtSize = int(srtHead.headers['content-length'])
-            flick.badSrt = 0
-        except KeyError:
-            errMsg("Key Error: " + flick.srtUrl)
-        try:
-            flick.fileSize = int(flickHead.headers['content-length'])
-            flick.badFile = 0 
-        except KeyError:
-            errMsg("Key Error: " + flick.downloadUrl)
-    else:
-        errMsg("Bad Request Code (ie 404): " + flick.downloadUrl)
+    flickHead = requests.head(flick.downloadUrl)
+    srtHead = requests.head(flick.srtUrl)
+    try:
+        flick.srtSize = int(srtHead.headers['content-length'])
+        flick.badSrt = False
+    except KeyError:
+        errMsg("Key Error: " + flick.srtUrl)
+    try:
+        flick.fileSize = int(flickHead.headers['content-length'])
+        flick.badFile = False 
+    except KeyError:
+        errMsg("Key Error: " + flick.downloadUrl)
 
 #Scrape info from html page and create flick object
 #Any content not found is simply skipped
@@ -73,55 +118,7 @@ def downloadInit(flick):
 def processFilm(flick):
     global link
     global base
-    flickUrl = flick.url
-    num = flick.num
-    filmReq = requests.get(flickUrl)
-    filmSource = filmReq.text
-    soup = BeautifulSoup(filmSource, "lxml")
-
-    for i in soup.find_all('div'):
-        if i.id == "error404":
-            skippedFlicks.append(flick)
-            return
-    if filmReq.status_code == requests.codes.ok:
-        try:
-            director = soup.find_all('div', {'class': ['director']})
-            try:
-                flick.director = director[0].a.contents[0]
-            except IndexError:
-                errMsg("Director not found: " + flickUrl)
-
-            table = soup.find_all('table')
-            try:
-                td = table[0].find_all('td')
-                try:
-                    flick.og = td[0].contents[1].strip()
-                except IndexError:
-                    errMsg("Original Title not found.")
-                try:
-                    flick.imdb = td[1].contents[1].strip()
-                except IndexError:
-                    errMsg("IMDB rating not found.")
-                #Number of views is td[2].contents[1] hence the jump
-                try:
-                    flick.year = td[3].contents[1].strip()
-                except IndexError:
-                    errMsg("Release Year not found.")
-                    return
-            except IndexError:
-                errMsg("Error finding <td>'s, skipping: " + flickUrl)
-                skippedFlicks.append(flick)
-
-        except AttributeError:
-            errMsg("Attribute Error, skipping: " + flickUrl)
-            skippedFlicks.append(flick)
-            return
-
-    else:
-        errMsg("Bad request code on flick page, skipping: " + flickUrl)
-        skippedFlicks.append(flick)
-        return
-
+    
     #Segmenting these strings more because I know these links aren't always perfect
     #ie if film has two parts it may be <num>-1.mp4 and <num>-2.mp4
     downloadBase = base + "movies/" + str(flick.num)
@@ -129,7 +126,60 @@ def processFilm(flick):
     flick.srtUrl = downloadBase + ".srt"
     downloadInit(flick)
 
-    if flick.badFile == 1:
+    flickUrl = flick.url
+    num = flick.num
+    filmReq = requests.get(flickUrl)
+    filmSource = filmReq.text
+    soup = BeautifulSoup(filmSource, "lxml")
+    for i in soup.find_all('div'):
+        if i.id == "error404":
+            skippedFlicks.append(flick)
+            return
+
+    try:
+        flickTitle = soup.find_all('h1')
+        flick.title = flickTitle[0].contents[0]
+    except IndexError:
+        errMsg("Flick Title not found: " + flickUrl)
+        skippedFlicks.append(flick)
+        return
+    if filmReq.status_code == requests.codes.ok:
+        try:
+            #May be able to pull director data from Flick text on page
+            #<div class="movie-description">...</div>
+            director = soup.find('div', {'class': ['director']}).contents[0].contents[0]
+            flick.director = director
+        except (IndexError, AttributeError):
+            errMsg("Director not found: " + flickUrl)
+
+        table = soup.find_all('table')
+        try:
+            td = table[0].find_all('td')
+            try:
+                flick.og = td[0].contents[1].strip()
+            except IndexError:
+                errMsg("Original Title not found.")
+            try:
+                flick.imdb = td[1].contents[1].strip()
+            except IndexError:
+                errMsg("IMDB rating not found.")
+            #Number of views is td[2].contents[1] hence the jump
+            try:
+                flick.year = td[3].contents[1].strip()
+            except IndexError:
+                errMsg("Release Year not found.")
+                return
+        except IndexError:
+            errMsg("Error finding <td>'s, skipping: " + flickUrl)
+            skippedFlicks.append(flick)
+    else:
+        errMsg("Bad request code on flick page, skipping: " + flickUrl)
+        skippedFlicks.append(flick)
+        return
+
+
+
+    if flick.badFile == True:
         skippedFlicks.append(flick)
     else:
         sovietMovies.append(flick)
@@ -170,20 +220,24 @@ def main():
             seen.add(x["num"])
 
     print("--- Processing Flicks ---")
-    for i in range(0, 1, 1):
-        tempFlick = Flick(links[i]["url"], links[i]["num"])
-        processFilm(tempFlick)
-
-    #for movie in links:
-    #    tempFlick = Flick(movie["url"], movie["num"])
+    #for i in range(0, 250, 1):
+    #    tempFlick = Flick(links[i]["url"], links[i]["num"])
     #    processFilm(tempFlick)
+
+    for movie in links:
+        tempFlick = Flick(movie["url"], movie["num"])
+        processFilm(tempFlick)
     
-    print("--- Flicks to Download ---")
-    for i in sovietMovies:
-        print(i)
-    print("--- Flicks to Skip ---")
-    for i in skippedFlicks:
-        print(i)
+    #print("--- Flicks to Download ---")
+    #for i in sovietMovies:
+    #    print(i)
+    #print("--- Flicks to Skip ---")
+    #for i in skippedFlicks:
+    #    print(i)
+    
+    #CSV delimited by bar '|'
+    print("--- Generating CSV File ---")
+    writeCsv()
 
 if __name__ == "__main__":
     main()
